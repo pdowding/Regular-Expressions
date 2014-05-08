09e8601c69777fcb4920ee39d812cfc2b2b88960
pdowding <phil_dowding@hotmail.com>
2014-05-08 15:16:58 -0500

    Initial commit

---
+++ spec/pig_latin_spec.rb
@@ -0,0 +1,14 @@
+require "../pig_latin.rb"
+
+
+it "has a phrase to be translated" do
+end
+
+context 'if the phrase starts with a vowel'
+
+context 'if the phrase starts with a consonant'
+
+context 'if the phrase starts with a y'
+
+
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/pig_latin.rb'
+require 'rspec'
+require_relative '../lib/spec/pig_latin_spec.rb'
